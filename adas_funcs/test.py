74bf517ff0951ee660c16c39d68debab138252ab
driveru <promitej.fadeev30@mail.ru>
2022-12-07 20:42:17 +0300

    Add test annotation and test run file

---
+++ adas_funcs/test.py
@@ -0,0 +1,11 @@
+from process_video import process_video
+
+def start_test_cloudy_1():
+    video_path = r"..\tests\cloudy_1.mp4"
+    annotation_path = r"..\tests\cloudy_1_annotation.json"
+    file_id = 197
+
+    process_video(video_path, annotation_path, file_id, debag=False)
+
+if __name__ == '__main__':
+    start_test_cloudy_1()
