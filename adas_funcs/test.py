08eaf3a38b35d4f0cfb8548441e80b34ea45c5f9
driveru <promitej.fadeev30@mail.ru>
2022-12-07 20:45:40 +0300

    Add test video

--- adas_funcs/test.py
+++ adas_funcs/test.py
@@ -1,7 +1,7 @@
 from process_video import process_video
 
 def start_test_cloudy_1():
-    video_path = r"..\tests\cloudy_1.mp4"
+    video_path = r"..\tests\cloudy_1_test.mp4"
     annotation_path = r"..\tests\cloudy_1_annotation.json"
     file_id = 197
 
